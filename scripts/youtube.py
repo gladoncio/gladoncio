e47f8424feb282a063d0f81f6dbfe3aa87eeee98
gladoncio <42326122+gladoncio@users.noreply.github.com>
2024-06-02 22:18:19 -0400

    Update youtube.py

--- scripts/youtube.py
+++ scripts/youtube.py
@@ -46,13 +46,9 @@ def main():
         videos.append((truncated_title, video_url, thumbnail_url))
 
 
-    markdown_content = "## Últimos Videos en YouTube"
-    markdown_content += '<div style="display: flex; justify-content: space-around;">\n'
+    markdown_content = "## Últimos Videos en YouTube\n\n"
     for title, url, thumbnail in videos:
-        markdown_content += f'  <div style="text-align: center; margin: 10px;">\n'
         markdown_content += f'    <a href="{url}"><img src="{thumbnail}" alt="{title}" width="30%"></a>'
-        markdown_content += '  </div>'
-    markdown_content += '</div>'
 
     # Lee el contenido existente del archivo
     with open(file_path, "r", encoding="utf-8") as file:
