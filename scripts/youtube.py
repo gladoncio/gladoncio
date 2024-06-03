65d20c993cc12bb81ff464ece2004998adfdeb14
gladoncio <42326122+gladoncio@users.noreply.github.com>
2024-06-02 22:03:20 -0400

    Update youtube.py

--- scripts/youtube.py
+++ scripts/youtube.py
@@ -37,8 +37,8 @@ def main():
         videos.append((title, video_url, thumbnail_url))
 
     # Define el tamaño de las imágenes
-    thumbnail_width = 200
-    thumbnail_height = 100
+    thumbnail_width = 220
+    thumbnail_height = 110
 
     # Genera el contenido en Markdown usando HTML
     markdown_content = "## Últimos Videos en YouTube\n\n"
